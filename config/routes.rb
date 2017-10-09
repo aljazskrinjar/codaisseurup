9d9a85b8a2082f83c06011fdab2bd49d1e45d282
aljazskrinjar <aljaz.skrinjar@gmail.com>
2017-10-09 14:27:30 +0200

    Add nav bar and homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-#  get 'pages/Home'
+  get 'pages/home'
 
-  root 'pages#home'
+ #get 'pages/Home'
+
+  root to: 'pages#home'
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details o the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
