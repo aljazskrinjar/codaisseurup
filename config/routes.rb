36e75524f44931fe476ccb1a2ee0ec4f8d8c4261
aljazskrinjar <aljaz.skrinjar@gmail.com>
2017-10-10 12:23:54 +0200

    first commit on the next day

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  #get 'pages/home'
 
  #get 'pages/Home'
 
   root to: 'pages#home'
   devise_for :users
+
+  resources :users, only: [:show]
+  resources :events, except: [:destroy]
   # For details o the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
